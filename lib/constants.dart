a9a1629906f2497002891cb04a7c1d1efa4b02ec
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2021-05-03 16:15:06 +0300

    add constants

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kBorderRadius = 16.0;
+const kShadow = BoxShadow(
+  color: Color.fromARGB(64, 00, 00, 16),
+  spreadRadius: 0,
+  blurRadius: 30,
+  offset: Offset(0, 8), // changes position of shadow
+);
