b9003182900815c3cc685341610d0b2880937eb4
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2021-05-18 23:46:07 +0300

    add id, formatted date getter

--- lib/data/models/launch.dart
+++ lib/data/models/launch.dart
@@ -1,17 +1,28 @@
-class Launch {
-  final String name;
-  final DateTime dateTime;
+import 'package:intl/intl.dart';
 
-  Launch({required this.name, required this.dateTime});
+class Launch {
+  final String id;
+  final String name;
+  final DateTime launchTimeUTC;
+  final DateFormat _dateFormat = DateFormat('MMMM d HH:mm');
+
+  Launch({required this.id, required this.name, required this.launchTimeUTC});
+
+  String get formattedLocalDate => _dateFormat.format(launchTimeUTC.toLocal());
 
   factory Launch.fromMap(Map<String, dynamic> map) {
     return Launch(
+      id: map['id'],
       name: map['name'],
-      dateTime: DateTime.parse(map['date_utc']),
+      launchTimeUTC: DateTime.parse(map['date_utc']),
     );
   }
 
   Map<String, dynamic> toMap() {
-    return {'name': name, 'date_utc': dateTime};
+    return {
+      'id': id,
+      'name': name,
+      'date_utc': launchTimeUTC,
+    };
   }
 }
