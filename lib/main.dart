bbcab9aecc32c2eb3b6313272ec3239c9a8a2207
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2021-05-24 01:04:23 +0300

    add bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'data/repositories/launches_repository.dart';
 import 'data/repositories/notifications_repository.dart';
 import 'logic/cubit/launches_cubit.dart';
-import 'view/widgets/launch_list_item.dart';
+import 'logic/cubit/subscribtions_cubit.dart';
+import 'view/widgets/launch_list.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,45 +24,32 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePage extends StatelessWidget {
   final LaunchesRepository _launchesRepo = LaunchesRepository();
-  late final LaunchesCubit _launchesCubit = LaunchesCubit(_launchesRepo);
-
-  @override
-  void initState() {
-    super.initState();
-    _launchesCubit.loadLaunches();
-  }
+  final NotificationsRepository _notifyRepo = NotificationsRepository();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<LaunchesCubit, LaunchesState>(
-        bloc: _launchesCubit,
-        builder: (_, state) {
-          if (state is LaunchesLoaded) {
-            return state.launches.isEmpty
-                ? Center(child: Text('No Launches Planned'))
-                : ListView.builder(
-                    physics: BouncingScrollPhysics(),
-                    itemCount: state.launches.length,
-                    itemBuilder: (_, i) {
-                      return LaunchListItem(
-                        image: i.isOdd
-                            ? 'assets/images/launch_1.jpg'
-                            : 'assets/images/launch_0.jpg',
-                        launch: state.launches[i],
-                      );
-                    },
-                  );
-          }
-          return Center(child: CircularProgressIndicator());
-        },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<LaunchesCubit>(
+          create: (_) => LaunchesCubit(_launchesRepo),
+        ),
+        BlocProvider<SubscribtionsCubit>(
+          create: (_) => SubscribtionsCubit(_notifyRepo),
+        ),
+      ],
+      child: Scaffold(
+        body: BlocBuilder<LaunchesCubit, LaunchesState>(
+          builder: (_, state) {
+            if (state is LaunchesLoaded) {
+              return state.launches.isEmpty
+                  ? Center(child: Text('No Launches Planned'))
+                  : LaunchList(launches: state.launches);
+            }
+            return Center(child: CircularProgressIndicator());
+          },
+        ),
       ),
     );
   }
